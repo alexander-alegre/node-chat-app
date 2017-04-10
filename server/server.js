63b6562ca7c4cacdf9d44f37c1d6686ca44d9200
= <alexander.alegre@outlook.com>
2017-04-09 20:14:47 -0700

    add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,17 +1,30 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-const app = express(); 
+var app = express(); 
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('new user connected');
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`http://localhost:${port}/`);
 });
