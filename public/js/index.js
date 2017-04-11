ff43502b37b3e4b8362e4c55df1c699f8b19f0b2
= <alexander.alegre@outlook.com>
2017-04-11 12:02:44 -0700

    add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -9,4 +9,21 @@ socket.on('disconenct', function() {
 
 socket.on('newMessage', function(message) {
     console.log('newMessage', message);
+    var li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    $('#messages').append(li);
+});
+
+
+
+$('#message-form').on('submit', function(e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message').val()
+    }, function() {
+
+    });
 });
